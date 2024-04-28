2481a90298078a339ed14440c235cdf476976e0d
Jean Rothstein <jean.machado@sou.inteli.edu.br>
2024-04-28 15:29:03 -0300

    ponderada2 2804

--- ponderada2/README.md
+++ ponderada2/README.md
@@ -7,3 +7,57 @@ Para rodar as duas aplicações é necessário rodar o arquivo `docker-compose.y
 docker compose up
 ```
 
+## Entrega 28/04 - Comparação entre Sanic e FastAPI para serviços de API REST
+
+Neste documento, é discutido as diferenças entre dois frameworks Python populares para desenvolvimento de serviços de API REST: Sanic e FastAPI. Abordaremos as diferenças em relação à assincronicidade e à velocidade de requisições, bem como as diferenças nas respostas oferecidas por esses frameworks.
+
+## Assincronicidade
+
+### Sanic
+Sanic é um framework Python assíncrono baseado no framework web Flask. Ele oferece suporte nativo para assincronicidade, permitindo que manipulemos várias solicitações de forma concorrente, o que pode levar a uma melhor escalabilidade e desempenho em aplicações com grande carga de requisições.
+
+### FastAPI
+FastAPI também é um framework Python assíncrono, mas é construído sobre o Starlette e o Pydantic. Ele utiliza a tipagem de dados do Python e a geração automática de documentação Swagger para fornecer uma experiência de desenvolvimento altamente produtiva. Assim como o Sanic, o FastAPI oferece suporte nativo para assincronicidade.
+
+## Velocidade de Requisições
+
+### Sanic
+Devido à sua natureza assíncrona e ao uso eficiente dos recursos do sistema, o Sanic geralmente é reconhecido por sua alta velocidade de processamento de requisições. Ele é capaz de lidar com um grande volume de solicitações simultâneas de forma eficiente e escalável.
+
+### FastAPI
+O FastAPI também é conhecido por sua alta velocidade de processamento de requisições. Ele aproveita ao máximo as características assíncronas do Python para lidar com várias solicitações simultâneas de forma eficiente. Além disso, sua integração com o Pydantic permite uma validação rápida e eficiente dos dados de entrada.
+
+## Diferença nas Respostas
+
+### Sanic
+Sanic fornece respostas rápidas e eficientes para solicitações HTTP. Ele permite definir rotas de forma simples e oferece uma ampla gama de opções para manipular as respostas, incluindo suporte para respostas JSON, HTML, arquivos estáticos, entre outros.
+
+### FastAPI
+FastAPI oferece um conjunto abrangente de ferramentas para manipulação de respostas HTTP. Ele suporta respostas JSON, HTML, arquivos estáticos e também possui uma integração fácil com sistemas de banco de dados e autenticação de usuários. Além disso, a geração automática de documentação Swagger facilita a compreensão e o uso das respostas fornecidas pela API.
+
+## Conclusão
+
+Tanto Sanic quanto FastAPI são excelentes escolhas para o desenvolvimento de serviços de API REST em Python. Ambos oferecem suporte nativo para assincronicidade e são conhecidos por sua alta velocidade de processamento de requisições. A escolha entre os dois dependerá das necessidades específicas do projeto e das preferências individuais do desenvolvedor.
+
+## Funcionalidades
+
+- **Usuários**: 
+  - Cadastrar novos usuários
+  - Logar usuários existentes
+  - Consultar informações de usuários
+  - Deletar usuários
+
+- **Tarefas**:
+  - Criar novas tarefas
+  - Atualizar tarefas existentes
+  - Deletar tarefas
+  - Consultar tarefas por ID de usuário
+
+## Observações
+
+Para o teste das requisições das funcionalidades acima com os serviços criados em Sanic e FastAPI, as colections do Insomnia foram disponibilizados.
+
+## Obs 2:
+
+Murilo, tentei integrar as aplicações com banco de dados Postgresql mas por algum motivo minha máquina não quis configurar, vou tentar arrumar isso essa semana, mas por enquanto estão rodando com banco de dados local...
+
--- ponderada2/app_Sanic/main.py
+++ ponderada2/app_Sanic/main.py
@@ -1,86 +1,133 @@
-# app/main.py
-
-from fastapi import FastAPI, Body, Depends
+from sanic import Sanic, response
+from sanic.request import Request
+from sanic.response import HTTPResponse, json
+from sanic_ext import Extend
 from auth.jwt_handler import signJWT
 from auth.jwt_bearer import jwtBearer
+from sanic.exceptions import ServerError
 
 from db import database, User, ToDo
 from models import ToDoSchema, UserSchema
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 
-app = FastAPI(title="API Rest nivel 2")
+app = Sanic("API-Rest-level-2")
+app.config.CORS_ORIGINS = "http://foobar.com,http://bar.com"
+Extend(app)
 
 
-@app.get("/")
-async def read_root():
-    return await User.objects.all()
+@app.middleware('request')
+async def jwt_middleware(request: Request):
+    if request.path not in ['/users/signup', '/users/login', '/', '/docs', '/docs/openapi.json', '/users/<id:int>' ]:
+        token = request.headers.get('Authorization')
+        if not token or not jwtBearer():
+            return json({"error": "Token inválido"}, status=401)
 
-@app.get("/users/", tags=["users"])
-async def read_user(id: int):
+
+@app.route("/")
+async def read_root(request: Request) -> HTTPResponse:
+    users = await User.objects.all()
+    user_dicts = [user.__dict__ for user in users]
+    return json(user_dicts)
+
+
+@app.route("/users/<id:int>", methods=["GET"])
+async def read_user(request: Request, id: int) -> HTTPResponse:
     if not database.is_connected:
         await database.connect()
-    return await User.objects.get(id=id)
+    user = await User.objects.get(id=id)
+    return json(user.__dict__)
 
-@app.get("/todo", tags=["todo"])
-async def read_todo(user_id: int):
+
+@app.route("/todo", methods=["GET"])    
+async def read_todo(request: Request) -> HTTPResponse:
+    #user_id = int(request.args.get("user_id"))
     if not database.is_connected:
         await database.connect()
-    return await ToDo.objects.all(user_id=user_id)
+    todos = await ToDo.objects.all()
+    todo_dicts = [todo.__dict__ for todo in todos]
+    return json(todo_dicts)
 
-@app.post("/users/signup", tags=["users"])
-async def create_user(user: UserSchema = Body(default=None)):
+
+@app.route("/users/signup", methods=["POST"])
+async def create_user(request: Request) -> HTTPResponse:
+    user = UserSchema(**request.json)
     if not database.is_connected:
         await database.connect()
     await User.objects.create(
         email=user.email,
         password=user.password
     )
-    return signJWT(user.email)
+    access_token = signJWT(user.email)
+    return json({"access_token": access_token})  # Retorna um dicionário com o token JWT
 
-@app.post("/users/login", tags=["users"])
-async def user_login(user: UserSchema = Body(default=None)):
-    if check_user(user):
-        return signJWT(user.email)
-    return {"error": "Usuário ou senha inválidos"}
-        
+
+
+@app.route("/users/login", methods=["POST"])
+async def user_login(request: Request) -> HTTPResponse:
+    user = UserSchema(**request.json)
+    if await check_user(user):
+        access_token = signJWT(user.email)
+        return json({"access_token": access_token})  # Retorna um dicionário com o token JWT
+    raise ServerError("Usuário ou senha inválidos", status_code=401)
+
+
+@app.route("/todo", methods=["POST"])
+async def create_todo(request: Request) -> HTTPResponse:
+    todo_data = request.json
+    title = todo_data.get("title")
+    content = todo_data.get("content")
+    user_id = todo_data.get("user_id")
     
-
-@app.post("/todo", tags=["todo"], dependencies=[Depends(jwtBearer())])
-async def create_todo(todo: ToDoSchema):
     if not database.is_connected:
         await database.connect()
-    return await ToDo.objects.create(
-        title=todo.title,
-        content=todo.content,
-        user_id=todo.user_id
-    )
+    
+    # Cria o objeto ToDo no banco de dados
+    todo = await ToDo.objects.create(title=title, content=content, user_id=user_id)
+    
+    # Retorna o objeto ToDo como dicionário serializável
+    todo_dict = todo.__dict__
+    return json(todo_dict, status=201)
 
-@app.put("/todo/update", tags=["todo"], dependencies=[Depends(jwtBearer())])
-async def update_todo(todo: ToDoSchema):
+
+@app.route("/todo/update", methods=["PUT"])
+async def update_todo(request: Request) -> HTTPResponse:
+    todo_data = request.json
+    todo_id = todo_data.get("id")
+    title = todo_data.get("title")
+    content = todo_data.get("content")
+    user_id = todo_data.get("user_id")
+    
     if not database.is_connected:
         await database.connect()
-    return await ToDo.objects.update_or_create(
-        id=todo.id,
-        title=todo.title,
-        content=todo.content,
-        user_id=todo.user_id
+    
+    # Atualiza ou cria o objeto ToDo no banco de dados
+    todo = await ToDo.objects.update_or_create(
+        id=todo_id,
+        title=title,
+        content=content,
+        user_id=user_id
     )
     
-@app.delete("/todo/delete/{id}", tags=["todo"], dependencies=[Depends(jwtBearer())])
-async def delete_todo(id: int):
+    # Retorna o objeto ToDo como dicionário serializável
+    todo_dict = todo.__dict__
+    return json(todo_dict)
+
+
+
+@app.route("/todo/delete/<id:int>", methods=["DELETE"])
+async def delete_todo(request: Request, id: int) -> HTTPResponse:
     if not database.is_connected:
         await database.connect()
-    return await ToDo.objects.delete(id=id)
+    return json(await ToDo.objects.delete(id=id))
 
-@app.delete("/users/delete/{id}", tags=["users"], dependencies=[Depends(jwtBearer())])
-async def delete_user(id: int):
+
+@app.route("/users/delete/<id:int>", methods=["DELETE"])
+async def delete_user(request: Request, id: int) -> HTTPResponse:
     if not database.is_connected:
         await database.connect()
-    return await User.objects.delete(id=id)
+    return json(await User.objects.delete(id=id))
 
 
-async def check_user(data:UserSchema):
+async def check_user(data: UserSchema):
     if not database.is_connected:
         await database.connect()
     users = await User.objects.all()
@@ -90,29 +137,16 @@ async def check_user(data:UserSchema):
     return False
 
 
-
-@app.on_event("startup")
-async def startup():
+@app.listener('before_server_start')
+async def before_start(app, loop):
     if not database.is_connected:
         await database.connect()
-    # create a dummy entry
-    # await User.objects.get_or_create(email="test@test.com", password="senha")
-    # await ToDo.objects.get_or_create(title="test", content="test", user_id=1)
 
 
-@app.on_event("shutdown")
-async def shutdown():
+@app.listener('after_server_stop')
+async def after_stop(app, loop):
     if database.is_connected:
         await database.disconnect()
 
-origins = ["*"]  # Substitua pelo URL do seu frontend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    app.run(host="0.0.0.0", port=8001, debug=True)
